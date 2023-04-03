a063e7b3c653d993d5158e1350ab827f4164460d
Chyniece Matthews <cfmatthews2@gmail.com>
2023-04-03 11:28:43 -0500

    Update Trivia.js

--- Trivia.js
+++ Trivia.js
@@ -4,27 +4,41 @@
 //save note card -- local storage?
 
 
-function Notecard(term,definition){
-    this.term = term;
-    this.definition = definition;
-    
-    
-    };
-    
-    renderNoteCard(){
-        showCard();
-        deleteCard();
-    };
-    function showCard(){
-    
-    };
-    
-    function deleteCard(){
-        if (event.target.innerHTML == "imgsource")
-        let 
-    };
-    
-    Notecard.prototype.addNoteCard(term,definition){
-    let newNotecard = new Notecard(term,definition)
-    
-    };
+const state = {
+    allNoteCards: [],
+    noteCardSets: null,
+};
+
+//notecard set constructor
+const noteCardSets = function (notecards) {
+    //this.notecards is an array of notecard instances
+    this.notecards = notecards;
+
+};
+
+const Notecard = function(term,definition){
+this.term = term;
+this.defintion = definition;
+};
+
+noteCardSets.prototype.addNoteCard = function(term, definition){
+   //this instance method creates a new notecard and adds it to this.notecards
+    let newNotecard = new Notecard(term, definition)
+    this.notecards.push(newNotecard);
+};
+
+noteCardSets.prototype.saveToLocalStorage = function () {
+    localStorage.setItem("notecard", JSON.stringify(Notecard));
+}
+renderNoteCard(){
+    showCard();
+    deleteCard();
+};
+function showCard() {
+
+};
+
+function deleteCard() {
+    if (event.target.innerHTML == "imgsource")
+        let
+};
