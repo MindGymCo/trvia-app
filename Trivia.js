743016bd6337537ebc306df41751b03e81d9e075
Chyniece Matthews <cfmatthews2@gmail.com>
2023-04-04 10:36:46 -0500

    deleted notecard from array

--- Trivia.js
+++ Trivia.js
@@ -13,30 +13,29 @@ const state = {
 const noteCardSets = function (notecards) {
     //this.notecards is an array of notecard instances
     this.notecards = notecards;
-
+    
 };
+
+let lastUsedId = 0; //give an id to the note cards to keep track of which are deleted in case two identical cards are created and deleted
 
 const Notecard = function(term,definition){
 this.term = term;
 this.defintion = definition;
+this.id = lastUsedId++;
 };
 
 noteCardSets.prototype.addNoteCard = function(term, definition){
    //this instance method creates a new notecard and adds it to this.notecards
     let newNotecard = new Notecard(term, definition)
     this.notecards.push(newNotecard);
+    state.allNoteCards.push(newNotecard); //adds the new notecard to the allnotecards array
 };
 
-/*noteCardSets.prototype.saveToLocalStorage = function () {
+noteCardSets.prototype.saveToLocalStorage = function () {
     localStorage.setItem("noteCardSets", JSON.stringify(state.notecardsets.notecards));
-};*/
+};
+noteCardSets.prototype.saveToLocalStorage();
 
-/*noteCardSets.prototype.deleteCard = function(notecards){
-    let filteredCards = this.notecards.filter(function(currentNoteCard){
-        return notecards.term !== currentNoteCard.Notecard.term
-    })
-    this.notecard = filteredCards;
-};*/
 
 
 function generateNote(term,definition){
@@ -48,10 +47,12 @@ return note;
 function showCardInTable(term,definition) { //add in note parameter
 let notecarddiv = document.getElementById('flashcardContainer')
 let notecard = document.createElement('div');
+let deleteicon = document.createElement('i');
+deleteicon.className = "fa fa-trash-can";
 notecard.classList.add('flashcard');
 notecard.innerText = term + "\n" + definition; // \n creates a new line
-
-notecarddiv.append(notecard)
+notecard.append(deleteicon);
+notecarddiv.append(notecard);
 };
 
 let form = document.querySelector('form');
@@ -63,24 +64,26 @@ form.addEventListener('submit',function(e){
     showCardInTable(term,definition);
 
 })
-//for the div that holds the notecards
-
-/*const table = document.getElementById('');
-table.addEventListener('click',deleteCard);*/
-
-/*function loadCardTable(){
-    const notecardTable = JSON.parse(localStorage.getItem('notecardsets')) || [];
-    state.notecardsets = new noteCardSets (Notecard)
-}*/
 
 
-/*renderNoteCard(){
-    loadCardTable();
-    showCard();
-    deleteCard();
-}*/
+const table = document.getElementById('flashcardContainer');
+table.addEventListener('click',deleteCard);
 
-/*function deleteCard(event) {
-    if (event.target.getElementById == "deletebutton")
-        let
-};*/
+function deleteCard(event) {
+    if (event.target.className == "fa fa-trash-can"){
+        const noteCardElement =  event.target.parentElement;
+        const id = noteCardElement.dataset.id; //get the id of the deleted notecard
+        const index = state.notecardsets.notecards.findIndex(noteCard=> noteCard.id == id); //finds the index of the note card in the notecards array
+        state.notecardsets.notecards.splice(index,1); //removes the deleted notecard from the notecards array
+        state.allNoteCards.splice(index,1); //removes the deleted notecard from the allNotecards array
+        noteCardElement.remove(); //removes the HTMl element from the page
+    }
+};
+
+
+
+
+
+
+
+
