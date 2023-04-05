89e20d58140a83717d3fc1cee05ce80d03cd56cc
Chyniece Matthews <cfmatthews2@gmail.com>
2023-04-05 16:14:52 -0500

    local storage

--- Trivia.js
+++ Trivia.js
@@ -15,11 +15,13 @@ const Notecard = function (term, definition) {
 function showCardInTable(term, definition, id) { //add in note parameter
     let notecarddiv = document.getElementById('flashcardContainer')
     let notecard = document.createElement('div');
+    let termAndDefinition = document.createElement('p');
     let deleteicon = document.createElement('i');
     deleteicon.className = "fa fa-trash-can";
     notecard.classList.add('flashcard');
-    notecard.innerText = term + "\n" + definition; // \n creates a new line
+    termAndDefinition.innerHTML = term + '\n' + definition;
     notecard.id = id;
+    notecard.append(termAndDefinition)
     notecard.append(deleteicon);
     notecarddiv.append(notecard);
 };
@@ -32,6 +34,9 @@ form.addEventListener('submit', function (e) {
     let definition = textareaoneandtwo[1].value
     let newNotecard = new Notecard(term, definition);
     state.allNoteCards.push(newNotecard);
+
+    localStorage.setItem("notecards", JSON.stringify(state.allNoteCards));
+
     showCardInTable(term, definition, newNotecard.id);
 
 })
@@ -58,18 +63,18 @@ function deleteCardFromArray(id) {
         else { return true }
     })
     state.allNoteCards = filteredArray;
+    localStorage.setItem("notecards", JSON.stringify(state.allNoteCards));
 }
-
 const searchDoc = document.querySelector('#searchbutton')
 searchDoc.addEventListener('click', function (e) {
     e.preventDefault();
 
-   const userInput = document.querySelector('#searchbar').value.toLowerCase(); //this will be where the user inputs their search
+    const userInput = document.querySelector('#searchbar').value.toLowerCase(); //this will be where the user inputs their search
     let searchOptions = document.querySelectorAll('.flashcard')
     for (let i = 0; i < searchOptions.length; i++) {
         //console.log(searchOptions[i].innerText.includes(userInput),userInput) 
 
-       if (searchOptions[i].innerText.includes(userInput)) {
+        if (searchOptions[i].innerText.includes(userInput)) {
             console.log(searchOptions[i].innerText)
             searchOptions[i].style.borderColor = 'yellow';
             console.log(searchOptions[i].style)
@@ -77,21 +82,35 @@ searchDoc.addEventListener('click', function (e) {
     }
 });
 
-function printNoteCards(table){
+function printNoteCards(table) {
     var printContents = table.innerHTML;
     var originalContent = document.body.innerHTML;
     document.body.innerHTML = printContents;
     window.print();
 
     document.body.innerHTML = originalContent;
+};
+
+function showAllNotecards() {
+    const table = document.getElementById('flashcardContainer');
+    state.allNoteCards = JSON.parse(localStorage.getItem("notecards")) || [];
+    table.innerHTML = '';
+    console.log(state.allNoteCards)
+    for (let i = 0; i < state.allNoteCards.length; i++) {
+        let term = state.allNoteCards[i].term
+        let definition = state.allNoteCards[i].definition
+        let id = state.allNoteCards[i].id;
+        showCardInTable(term, definition, id);
+        //localStorage.setItem('table',JSON.stringify(table))
+        console.log(state.allNoteCards[i])
+    }
 }
 
+showAllNotecards();
 
 
-/*saveToLocalStorage = function () {
-    localStorage.setItem("noteCardSets", JSON.stringify(state.notecardsets.notecards));
-};
-saveToLocalStorage(); -- save filtered array to local storage, save new note card to local storage */
+
+ //-- save filtered array to local storage, save new note card to local storage
 
 
 
